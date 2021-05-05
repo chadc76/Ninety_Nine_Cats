9713ad7732341c447074eda5ba32e516b9bd2f8c
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-05 15:57:03 -0400

    Added Model validations for presence and inclusion

---
+++ app/models/cat_rental_request.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: cat_rental_requests
+#
+#  id         :bigint           not null, primary key
+#  cat_id     :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#  status     :string           default("PENDING")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class CatRentalRequest < ApplicationRecord
+  validates :status, inclusion: %w(PENDING APPROVED DENIED)
+  validates :cat_id, :start_date, :end_date, presence: true
+
+  
+end
