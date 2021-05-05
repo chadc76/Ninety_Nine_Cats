b60185d91335fff25651bedeb067ce21750b02a8
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-05 14:46:03 -0400

    Added new and create actions and cat_params

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,20 @@ class CatsController < ApplicationController
     @cat = Cat.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    @cat.save ? (redirect_to cat_url(@cat)) : (render :new)
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :age, :sex, :description)
+  end
 end
