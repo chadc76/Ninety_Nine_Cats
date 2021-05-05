40571ef0398142d3156589b82547de6fd1eef693
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-05 15:29:43 -0400

    Added edit and update actions

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -19,6 +19,16 @@ class CatsController < ApplicationController
     @cat.save ? (redirect_to cat_url(@cat)) : (render :new)
   end
 
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    @cat.update(cat_params) ? (redirect_to cat_url(@cat)) : (render :edit)
+  end
+
   private
 
   def cat_params
