e2dd4c4ea6beb37c2e6e9b57445e2a861f9d287f
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-05 17:38:14 -0400

    Added overlapping requests validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -11,16 +11,47 @@
 #  updated_at :datetime         not null
 #
 class CatRentalRequest < ApplicationRecord
-  validates :status, inclusion: %w(PENDING APPROVED DENIED)
+  STATUS_STATES = %w(APPROVED DENIED PENDING).freeze
+
   validates :cat_id, :start_date, :end_date, presence: true
-  after_destroy :log_destroy_action
+  validates :status, inclusion: STATUS_STATES
+  validate :does_not_overlap_approved_request
 
   belongs_to :cat,
     primary_key: :id,
     foreign_key: :cat_id,
     class_name: :Cat
 
-  def log_destroy_action
-    puts 'Rental Request Destroyed'
+  def approved?
+    self.status == "APPROVED"
+  end
+
+  def pending?
+    self.status == "PENDING"
+  end
+
+  def denied?
+    self.status == "DENIED"
+  end
+
+  private
+
+  def overlapping_requests
+    CatRentalRequest
+      .where.not(id: self.id)
+      .where(cat_id: cat_id)
+      .where.not('start_date > :end_date OR end_date < :start_date', start_date: start_date, end_date: end_date)
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where('status = \'APPROVED\'')
+  end
+
+  def does_not_overlap_approved_request
+    return true if self.denied?
+
+    unless !overlapping_approved_requests.exists?
+      errors.add(:base, message: 'Request conflicts with existing approved request')
+    end
   end
 end
