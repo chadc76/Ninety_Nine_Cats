9c242aa4123074a1866be133efa791d1f42cdb77
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-05 23:24:31 -0400

    Fixed errors

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,16 +1,15 @@
 class CatRentalRequestsController < ApplicationController
   def new
     @request = CatRentalRequest.new
-    @cats = Cat.all.sort_by(:id)
     render :new
   end
 
   def create
     @request = CatRentalRequest.new(cat_rental_request_params)
-    @request.save ? redirect_to cat_url(@request.cat_id) : render :new
+    @request.save ? (redirect_to cat_url(@request.cat_id)) : (render :new)
   end
 
   def cat_rental_request_params
-    params.require(:cat_rental_reques).permit(:cat_id, :start_date, :end_date)
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date)
   end
 end
