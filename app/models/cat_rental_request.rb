339260a3aad5094f2d5251a014907640057b45f5
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-12 17:24:21 -0400

    Made look like Skeleton

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -6,58 +6,59 @@
 #  cat_id     :integer          not null
 #  start_date :date             not null
 #  end_date   :date             not null
-#  status     :string           default("PENDING")
+#  status     :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class CatRentalRequest < ApplicationRecord
   STATUS_STATES = %w(APPROVED DENIED PENDING).freeze
 
-  validates :cat_id, :start_date, :end_date, presence: true
+  validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: STATUS_STATES
+  validate :start_must_come_before_end
   validate :does_not_overlap_approved_request
-  validate :starts_before_end_date
 
   belongs_to :cat,
     primary_key: :id,
     foreign_key: :cat_id,
     class_name: :Cat
 
-  def approve!
-    if pending?
-      CatRentalRequest.transaction do 
-        self.status = "APPROVED" 
-        self.save
-        overlapping_pending_requests.each do |request| 
-          request.status = "DENIED"
-          request.save
-        end
-      end
-    end
-  end
+  after_initialize :assign_pending_status
 
-  def deny!
-    if pending?
-      CatRentalRequest.transaction do 
-        self.status = "DENIED" 
-        self.save
+  def approve!
+    raise 'not pending' unless self.status == 'PENDING'
+    transaction do 
+      self.status = "APPROVED" 
+      self.save
+
+      overlapping_pending_requests.each do |req|update! 
+        req.update!(status: 'DENIED')
       end
     end
   end
 
   def approved?
-    self.status == "APPROVED"
-  end
-
-  def pending?
-    self.status == "PENDING"
+    self.status == 'APPROVED'
   end
 
   def denied?
-    self.status == "DENIED"
+    self.status == 'DENIED'
+  end
+
+  def deny!
+    self.status = 'DENIED'
+    self.save!
+  end
+
+  def pending?
+    self.status == 'PENDING'
   end
 
   private
+
+  def assign_pending_status
+    self.status ||= 'PENDING'
+  end
 
   def overlapping_requests
     CatRentalRequest
@@ -75,17 +76,16 @@ class CatRentalRequest < ApplicationRecord
   end
 
   def does_not_overlap_approved_request
-    return true if self.denied?
+    return if self.denied?
 
-    unless !overlapping_approved_requests.exists?
+    unless overlapping_approved_requests.empty?
       errors.add(:base, message: 'Request conflicts with existing approved request')
     end
   end
 
-  def starts_before_end_date
-    unless start_date < end_date
-      errors.add(:start_date, message: 'must come before end date')
-      errors.add(:end_date, message: 'must come after start date')
-    end
+  def start_must_come_before_end
+    return if start_date < end_date
+    errors[:start_date] << 'must come before end date'
+    errors[:end_date] << 'must come after start date'
   end
 end
