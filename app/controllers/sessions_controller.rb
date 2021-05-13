ec16d3a42faaaa6b09de4fbbb4642791e6093f7c
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-13 12:00:15 -0400

    Fixed error message and used login! method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
+  before_action :current_user?, except: [:destroy]
+
   def create
     user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
     )
 
+
+
     if user.nil?
-      flash.now[:error] = "Credentials were wrong"
+      flash.now[:errors] = ["Username and Password did not match"]
       render :new
     else
-      user.reset_session_token!
-      session[session_token] = user.session_token
+      login!(user)
       redirect_to cats_url
     end
   end
@@ -23,6 +26,7 @@ class SessionsController < ApplicationController
     return if !current_user
     current_user.reset_session_token!
     session[:session_token] = nil
+    redirect_to root_url
   end
 
 end
