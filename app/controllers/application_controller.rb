cf68def71281aea7ef32d443f2e3b39370716aa3
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-13 12:52:24 -0400

    Added require_current_user, is_owner?, current_cat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,22 @@ class ApplicationController < ActionController::Base
   def current_user?
     redirect_to cats_url if !current_user.nil?
   end
+
+  def require_current_user!
+    if current_user.nil?
+      flash[:errors] = ["You must be Logged in to continue"]
+      redirect_to new_session_url
+    end
+  end
+
+  def is_owner?
+    unless current_user.id == current_cat.user_id
+      flash[:errors] = ["You can\'t edit a Cat you don\'t own"]
+      redirect_to cat_url(current_cat) 
+    end
+  end
+
+  def current_cat
+    @current_cat ||= Cat.find_by(id: params[:id])
+  end
 end
