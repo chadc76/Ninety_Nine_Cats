4c7024ee3304cef281b8790911132f26690ef80a
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-14 19:32:23 -0400

    added a ne w helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,12 @@ module ApplicationHelper
     button_to "Edit #{@cat.name}", edit_cat_url(@cat), method: :get if cat_owner?
   end
 
+  def session_env(token)
+    html = token.token == session[:session_token] ? "<td>This Device</td>" : "<td>h(token.env)</td>"
+    html.html_safe
+  end
+
   def cat_owner?
-    current_user && @cat.user_id == current_user.id
+    @cat.owner == current_user
   end
 end
