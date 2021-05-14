02916ad6b24773e3b6c294fc330a37d225def445
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-14 18:20:35 -0400

    Added helper methods for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def edit_button
+    button_to "Edit #{@cat.name}", edit_cat_url(@cat), method: :get if cat_owner?
+  end
+
+  def cat_owner?
+    current_user && @cat.user_id == current_user.id
+  end
 end
