2d7ca512ecb9d33aed72533d07a3e85aff66f730
Chad Cunningham <chadcunningham7676@gmail.com>
2021-05-20 02:13:43 -0400

    Added faker to gem file and updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,37 +10,31 @@ User.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('users')
 ApplicationRecord.connection.reset_pk_sequence!('session_tokens')
 ApplicationRecord.connection.reset_pk_sequence!('user_sessions')
-puts "Creating Users"
-User.create!(username: "Houdini", password: "i_like_kiki" )
-User.create!(username: "houdini", password: "i_love_kiki")
-User.create!(username: "Drake", password: "OnlyLoveMyBedAndMyMomma")
-User.create!(username: "Lil_Wayne", password: "YoungMoolaBaby")
-User.create!(username: "Chance_The_Rapper", password: "TheBigDay")
-
 Cat.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('cats')
-puts "Creating Cats"
-Cat.create!(user_id: 1, name: "Tattooed", birth_date: "2018/08/06", color: "orange", sex: "F")
-Cat.create!(user_id: 1, name: "Fixie", birth_date: "2020/02/06", color: "white", sex: "F")
-Cat.create!(user_id: 2, name: "Tacos", birth_date: "2017/04/24", color: "white", sex: "F")
-Cat.create!(user_id: 2, name: "Fingerstache", birth_date: "2011/10/09", color: "orange", sex: "M")
-Cat.create!(user_id: 3, name: "Cardigan", birth_date: "2017/08/02", color: "orange", sex: "F")
-Cat.create!(user_id: 3, name: "Flannel", birth_date: "2018/09/30", color: "white", sex: "M")
-Cat.create!(user_id: 4, name: "YR", birth_date: "2014/04/15", color: "orange", sex: "M")
-Cat.create!(user_id: 4, name: "Forage", birth_date: "2013/01/27", color: "white", sex: "F")
-Cat.create!(user_id: 5, name: "Thundercats", birth_date: "2020/11/20", color: "brown", sex: "M")
-Cat.create!(user_id: 5, name: "Hella", birth_date: "2017/08/17", color: "brown", sex: "F")
-
 CatRentalRequest.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('cat_rental_requests')
-puts "Creating Cat Rental Requests"
-CatRentalRequest.create!(user_id: 5, cat_id: 1, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 5, cat_id: 2, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 4, cat_id: 3, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 4, cat_id: 4, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 2, cat_id: 5, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 2, cat_id: 6, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 3, cat_id: 7, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 3, cat_id: 8, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 1, cat_id: 9, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
-CatRentalRequest.create!(user_id: 1, cat_id: 10, start_date: "2021/5/11", end_date: "2021/5/18", status: "PENDING")
+
+last_year = Date.today.prev_year.year
+
+10.times do
+  puts "Creating User"
+  u = User.create!(username: Faker::Name.name, password: "password")
+  puts "Creating Cat"
+  c = Cat.create!(
+    user_id: u.id,
+    name: Faker::Hipster.word,
+    color: Cat::CAT_COLORS.sample,
+    sex: ["M", "F"].sample,
+    description: Faker::Hipster.sentence,
+    birth_date: Faker::Date.between(from: 10.years.ago, to: Date.today)
+    )
+  puts "Creating Cat Rental Request"
+    CatRentalRequest.create!(
+    cat_id: Cat.last.id,
+    user_id: User.last.id,
+    start_date: Date.parse("#{last_year.to_s}-01-01"),
+    end_date: Date.parse("#{last_year.to_s}-01-28"),
+    status: 'APPROVED'
+  )
+end
